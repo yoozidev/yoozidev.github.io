b4921075a2eb19a3b7c9f2fd023e0b87695c6f9e
yoozidev <yoozidev@gmail.com>
2022-06-03 14:21:59 +0900

    contact_made_table

--- src/app/content/contact/contact.component.ts
+++ src/app/content/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpHeaders} from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -9,13 +10,21 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ContactComponent implements OnInit {
 
+  contactForm = new FormGroup({
+    name: new FormControl(''),
+    subject: new FormControl(''),
+    replyto: new FormControl(''),
+    message: new FormControl('')
+  });
+
   constructor(
     private http: HttpClient
   ) { }
 
   ngOnInit(): void {
   }
-  onSubmit() {
+  
+  submit() {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     this.http.post('https://formspree.io/f/xrgjjvak',
       { name: 'name', subject: 'subject', replyto: 'email', message: 'message' },
